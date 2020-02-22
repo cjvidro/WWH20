e486260eec989df0c3cd546cb40f52fc987555e7
cjvidro <cjvidro@mtu.edu>
2020-02-21 22:55:15 -0500

    General Styling of home page

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("student.fxml"));
         primaryStage.setTitle("Dev");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1024, 576));
         primaryStage.show();
     }
 
