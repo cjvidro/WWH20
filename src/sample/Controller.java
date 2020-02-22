af2102ef57ca8af0e38a9fb0212d4606e4249e4e
justmarsh <jlmarsha@mtu.edu>
2020-02-22 01:49:31 -0500

    Added ability to change between scenes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,18 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,11 +24,13 @@ public class Controller
 
     private String password;
 
+    /* Keep track of all the tasks via ArrayLists */
     private ArrayList<String> school;
     private ArrayList<String> chores;
     private ArrayList<String> sports;
     private ArrayList<String> other;
 
+    /* VBoxes to display tasks to be done */
     @FXML
     private VBox schoolPane;
     @FXML
@@ -33,12 +42,7 @@ public class Controller
 
 
     @FXML
-    private TextField enterPassword;
-
-
-    @FXML
-    private TextArea output;
-
+    private PasswordField enterPassword;
 
     @FXML
     private URL location;
@@ -52,7 +56,7 @@ public class Controller
         password = "1234";
         parent = false;
         // Test values for the ArrayLists.
-        school = new ArrayList<String>();
+        school = new ArrayList<>();
         chores = new ArrayList<>();
         sports = new ArrayList<>();
         other = new ArrayList<>();
@@ -66,57 +70,136 @@ public class Controller
     @FXML
     private void initialize()
     {
-        TextField text = new TextField();
-        text.setText("WORDS ON THE SCREEN!\n");
-        schoolPane.getChildren().add(text);
-        CheckBox tasks;
-        String test[] = {"Art", "Math"};
-        for(int i = 0; i < 2; i++)
-        {
-            tasks = new CheckBox(test[i]);
-            schoolPane.getChildren().add(tasks);
-        }
+        schoolPane = new VBox();
+        chorePane = new VBox();
+        sportPane = new VBox();
+        otherPane = new VBox();
+        System.out.println("Entered initialize.\n");
+        outputSchool();
+        outputChore();
+        outputSport();
+        outputOther();
 
     }
 
-
-    @FXML
-    private void test()
-    {
-
-    }
-
+    /* ============================== Login Section ======================================== */
 
     // For logging into the parent account.
     @FXML
-    private void logIn()
+    private void login(ActionEvent event)
     {
         parent = password.equals(enterPassword.getText());
-        outputLogged();
     }
 
-    private void outputLogged()
-    {
-        if(!parent)
-        {
-            output.setText("Password is incorrect.\n");
-        }
-        else
-        {
-            output.setText("Logged in.\n");
-        }
-    }
-
-    @FXML
-    private void outputSchool()
-    {
-
-
-    }
-    // Checks to see if they are logged in.S
+    // Checks to see if they are logged in.
     private boolean loggedIn()
     {
         return parent;
     }
+
+
+    /* ===================================================================================== */
+
+
+    /* ============================= Output into VBoxes ==================================== */
+    @FXML
+    private void outputSchool()
+    {
+        CheckBox temp;
+        for(String element: school)
+        {
+            temp = new CheckBox(element);
+            if(temp != null)
+            {
+                schoolPane.getChildren().add(temp);
+            }
+        }
+
+    }
+
+    @FXML
+    private void outputChore()
+    {
+        CheckBox temp;
+        for(String element: chores)
+        {
+            temp = new CheckBox(element);
+            if(temp != null)
+            {
+                chorePane.getChildren().add(temp);
+            }
+        }
+
+    }
+    @FXML
+    private void outputSport()
+    {
+        CheckBox temp;
+        for(String element: sports)
+        {
+            temp = new CheckBox(element);
+            if(temp != null)
+            {
+                sportPane.getChildren().add(temp);
+            }
+        }
+
+    }
+    @FXML
+    private void outputOther()
+    {
+        CheckBox temp;
+        for(String element: other)
+        {
+            temp = new CheckBox(element);
+            if(temp != null)
+            {
+                otherPane.getChildren().add(temp);
+            }
+        }
+
+    }
+    /* ===================================================================================== */
+
+    /* =================================== Change Scenes =================================== */
+    // Changes the scene to the login view
+    @FXML
+    private void LoginScene(ActionEvent event) throws IOException
+    {
+        Parent loginView = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Scene loginScene = new Scene(loginView);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(loginScene);
+        window.show();
+    }
+
+    @FXML
+    private void StudentScene(ActionEvent event) throws IOException
+    {
+        Parent studentView = FXMLLoader.load(getClass().getResource("student.fxml"));
+        Scene studentScene = new Scene(studentView);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(studentScene);
+        window.show();
+
+    }
+
+    @FXML
+    private void StatScene(ActionEvent event) throws IOException
+    {
+
+    }
+
+    @FXML
+    private void ParentScene(ActionEvent event) throws IOException
+    {
+
+    }
+
+    /* ===================================================================================== */
 }
 
