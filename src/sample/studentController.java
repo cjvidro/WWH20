dadb65125948feaf55216ac25b63613b2fa17365
cjvidro <cjvidro@mtu.edu>
2020-02-22 13:37:19 -0500

    Lots of CSS

--- src/sample/studentController.java
+++ src/sample/studentController.java
@@ -89,7 +89,6 @@ public class studentController
         for(tasks element: school)
         {
             checkbox = new CheckBox(element.getTask());
-            checkbox.setStyle("-fx-font-size: 18px; -fx-font-family: 'Segoe UI Semibold'; -fx-text-fill: WHITE;");
 
             if(checkbox != null)
             {
@@ -106,7 +105,6 @@ public class studentController
         for(tasks element: chores)
         {
             checkbox = new CheckBox(element.getTask());
-            checkbox.setStyle("-fx-font-size: 18px; -fx-font-family: 'Segoe UI Semibold'; -fx-text-fill: WHITE;");
             if(checkbox != null)
             {
                 chorePane.getChildren().add(checkbox);
@@ -121,7 +119,6 @@ public class studentController
         for(tasks element: sports)
         {
             checkbox = new CheckBox(element.getTask());
-            checkbox.setStyle("-fx-font-size: 18px; -fx-font-family: 'Segoe UI Semibold'; -fx-text-fill: WHITE;");
             if(checkbox != null)
             {
                 sportPane.getChildren().add(checkbox);
@@ -136,7 +133,6 @@ public class studentController
         for(tasks element: other)
         {
             checkbox = new CheckBox(element.getTask());
-            checkbox.setStyle("-fx-font-size: 18px; -fx-font-family: 'Segoe UI Semibold'; -fx-text-fill: WHITE;");
             if(checkbox != null)
             {
                 otherPane.getChildren().add(checkbox);
